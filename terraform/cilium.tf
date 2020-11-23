d96dc0498611511bed078cd57efb65e013a3f525
Pierre Ugaz <ulm0@innersea.xyz>
2020-11-23 16:47:55 -0300

    chore: cilium 1.9.0, k3s 1.9.4

--- terraform/cilium.tf
+++ terraform/cilium.tf
@@ -4,83 +4,71 @@ resource helm_release cilium {
   atomic     = true
   repository = "https://helm.cilium.io"
   chart      = "cilium"
-  version    = "1.8.4"
+  version    = "1.9.0"
   namespace  = "kube-system"
   set {
-    name  = "global.hubble.metrics.enabled"
+    name  = "hubble.metrics.enabled"
     value = "{dns,drop,tcp,flow,port-distribution,icmp,http}"
   }
   values = [
     yamlencode(
       {
-        config = {
-          ipam = "kubernetes"
+        ipam = {
+          mode = "kubernetes"
         }
-        global = {
-          bpf = {
-            monitorAggregation = "maximum"
-            preallocateMaps    = true
-            waitForMount       = true
-          }
-          cluster = {
-            name = yamldecode(data.external.kubeconfig.result.kubeconfig).clusters.0.name
-          }
-          containerRuntime = {
-            integration = "containerd"
-            socketPath  = "/var/run/k3s/containerd/containerd.sock"
-          }
-          debug = {
-            enabled = true
-          }
-          device = "eth0"
-          externalIPs = {
-            enabled = true
-          }
-          hostPort = {
-            enabled = true
-          }
-          hostServices = {
-            enabled = true
-          }
-          hubble = {
-            enabled       = true
-            listenAddress = ":4244"
-            relay = {
-              enabled = true
-            }
-            ui = {
-              enabled = true
-            }
-          }
-          k8s = {
-            requireIPv4PodCIDR = true
-          }
-          k8sServiceHost       = libvirt_domain.server.network_interface.0.addresses.0
-          k8sServicePort       = 6443
-          kubeProxyReplacement = "strict"
-          # nodeinit = {
-          #   enabled = true
-          # }
-          nodePort = {
-            enabled = true
-            mode    = "hybrid"
-          }
-          operatorPrometheus = {
-            enabled = true
-          }
-          prometheus = {
-            enabled = true
-          }
-          psp = {
-            enabled = true
-          }
-          tunnel = "geneve"
+        debug = {
+          enabled = true
         }
-        # nodeinit = {
-        #   restartPods = var.cilium_node_init_restart_pods
-        # }
+        k8sServiceHost = libvirt_domain.server.network_interface.0.addresses.0
+        k8sServicePort = 6443
+        k8s = {
+          requireIPv4PodCIDR = true
+        }
+        priorityClassName = "system-cluster-critical"
+        bpf = {
+          monitorAggregation = "maximum"
+          preallocateMaps    = true
+          waitForMount       = true
+        }
+        containerRuntime = {
+          integration = "containerd"
+          socketPath  = "/var/run/k3s/containerd/containerd.sock"
+        }
+        nodePort = {
+          enabled = true
+        }
+        prometheus = {
+          enabled = true
+          serviceMonitor = {
+            enabled = false
+          }
+        }
+        cluster = {
+          name = yamldecode(data.external.kubeconfig.result.kubeconfig).clusters.0.name
+        }
+        externalIPs = {
+          enabled = true
+        }
+        hostPort = {
+          enabled = true
+        }
+        hostServices = {
+          enabled = true
+        }
+        hubble = {
+          enabled       = true
+          listenAddress = ":4244"
+          relay = {
+            enabled = true
+          }
+          ui = {
+            enabled = true
+          }
+        }
+        kubeProxyReplacement = "strict"
+        tunnel               = "geneve"
         operator = {
-          numReplicas = 1
+          replicas = 1
         }
       }
     )
