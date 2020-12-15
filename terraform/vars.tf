a23ca991ed77d1494d9744925c74cc9bd4c868d6
Pierre Ugaz <ulm0@innersea.xyz>
2020-12-15 12:12:08 -0300

    chore: k3s 1.19.5

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,6 @@
 variable agents { default = 1 }
-variable k3skit_kernel { default = "https://github.com/ulm0/k3skit/releases/download/v1.19.4-k3s1/k3s-kernel" }
-variable k3skit_os { default = "https://github.com/ulm0/k3skit/releases/download/v1.19.4-k3s1/k3s-initrd.img" }
+variable k3skit_kernel { default = "https://github.com/ulm0/k3skit/releases/download/v1.19.5-k3s1/k3s-kernel" }
+variable k3skit_os { default = "https://github.com/ulm0/k3skit/releases/download/v1.19.5-k3s1/k3s-initrd.img" }
 variable libvirt_pool { default = "/tmp/libvirt-pool-k3skit" }
 variable private_key_filename { default = "./default.pem" }
+
